7dd0cd7a8b891aa3568f30c4dc9cefc6c75ce988
bgsrio <bgsrio@gmail.com>
2022-10-23 16:28:03 -0300

    nesse já fui um pouquinho melhor do que no anterior

--- src/movies.js
+++ src/movies.js
@@ -1,16 +1,41 @@
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
-function getAllDirectors(moviesArray) {}
+function getAllDirectors(moviesArray) {
+    return moviesArray.map(currentMovie => currentMovie.director);
+  }
+   console.log(getAllDirectors(movies));
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-function howManyMovies(moviesArray) {}
+function howManyMovies(moviesArray) {
+    return moviesArray.filter(currentMovie => currentMovie.genre.includes("Drama") && currentMovie.director === "Steven Spielberg").length;
+}
+ console.log(howManyMovies(movies));
+   
 
-// Iteration 3: All scores average - Get the average of all scores with 2 decimals
-function scoresAverage(moviesArray) {}
+// Iteration 3  : All scores average - Get the average of all scores with 2 decimals
+function scoresAverage(moviesArray) {
+      if (moviesArray.length === 0) return 0;
+
+      let average = moviesArray.reduce((result, currentMovie) => result + (currentMovie.score || 0), 0);
+      return Math.round(average / moviesArray.length * 100) / 100
+  }
+    console.log (scoresAverage(movies));
+
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
-function dramaMoviesScore(moviesArray) {}
+function dramaMoviesScore(moviesArray) {
+
+    const dramaMovies = moviesArray.filter(currentMovie => currentMovie.genre.includes("Drama"));
+
+    if (dramaMovies.length === 0) return 0;
+
+    let average = dramaMovies.reduce((result, currentMovie) => result + (currentMovie.score || 0), 0);
+  return Math.round(average / dramaMovies.length * 100) / 100;
+}
+
+console.log (dramaMoviesScore(movies));
+
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
 function orderByYear(moviesArray) {}
